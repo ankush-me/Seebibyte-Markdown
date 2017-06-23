953bddf7617ea7b56901c7f390f62a03d3f4ca23
nicechay <nstar.garg@gmail.com>
2017-06-23 19:32:05 +0530

    carousel

---
+++ scripts/custcard.js
@@ -0,0 +1,30 @@
+function custCard(el) {
+	this.el = el;
+	this.ul = this.el.getElementsByTagName("ul")[0];
+	this.li = this.el.getElementsByTagName("li");
+	this.idx = 0;
+	this.len = this.li.length;
+	this.div = [];
+	this.liw = this.li[0].clientWidth;
+	this.ulw = this.ul.clientWidth;
+	this.cw = 0;
+	console.log(this.cw);
+	console.log(this.liw);
+	console.log(this.ulw);
+	
+}
+
+custCard.prototype.animator = function() {
+	this.ul.style.transform = "translate3d(-"+this.cw+"px,0,0)";
+	this.cw = (this.cw+this.liw)%this.ulw;
+	
+};
+
+
+
+
+var cc = new custCard(document.getElementsByClassName("custCard")[0]);
+
+setInterval(function() {
+	cc.animator();
+},2000);
