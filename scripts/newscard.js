c4a74ee8be0ab3790de9dff653067137065f6799
nicechay <nstar.garg@gmail.com>
2017-07-03 19:25:17 +0530

    Inline Fix

--- scripts/newscard.js
+++ scripts/newscard.js
@@ -12,7 +12,7 @@ newsCard.prototype.divify = function() {
 	this.el.classList.add("newsCard");
 	this.el.innerHTML = '<div data-index="1" ><p class="cover"><img src="'+
 	this.cover+'" alt="'+this.title+'" /></p><div class="infodiv" ><p class="title" >'+this.title+'</p><p class="date" >'+this.date+'</p><p class="highlight">'+
-	this.highlight+'</p></div></div><div data-index="2" ><span><a href="https://twitter.com/share" class="twitter-share-button" data-size="large" data-text="'+this.title+'\n" data-url="'+window.location.href+'" data-dnt="true" data-show-count="false"></a></span><span data-act="1" class="icon icon-carat-down" ></span></div><div data-index="3" ><p class="long hide">'+
+	this.highlight+'</p><div data-index="2" ><span><a href="https://twitter.com/share" class="twitter-share-button" data-size="large" data-text="'+this.title+'\n" data-url="'+window.location.href+'" data-dnt="true" data-show-count="false"></a></span><span data-act="1" class="icon icon-carat-down" ></span></div></div></div><div data-index="3" ><p class="long hide">'+
 	this.content+'</p></div>';
 	
 	var that = this;
