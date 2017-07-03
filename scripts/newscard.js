3b606662b7672781337e9e81bc6071896c585a51
nicechay <nstar.garg@gmail.com>
2017-07-03 08:25:22 +0530

    tweet button

--- scripts/newscard.js
+++ scripts/newscard.js
@@ -12,13 +12,14 @@ newsCard.prototype.divify = function() {
 	this.el.classList.add("newsCard");
 	this.el.innerHTML = '<div data-index="1" ><p class="cover"><img src="'+
 	this.cover+'" alt="'+this.title+'" /></p><div class="infodiv" ><p class="title" >'+this.title+'</p><p class="date" >'+this.date+'</p><p class="highlight">'+
-	this.highlight+'</p></div></div><div data-index="2" ><span><a target="_blank" href="https://twitter.com/intent/tweet?text='+encodeURI(this.title+'\nRead at :&nbsp '+window.location.href)+'" >Tweet</a></span><span data-act="1" >Read</span></div><div data-index="3" ><p class="long hide">'+
+	this.highlight+'</p></div></div><div data-index="2" ><span><a href="https://twitter.com/share" class="twitter-share-button" data-size="large" data-text="'+this.title+'\n" data-url="'+window.location.href+'" data-dnt="true" data-show-count="false"></a></span><span data-act="1" class="icon icon-carat-down" ></span></div><div data-index="3" ><p class="long hide">'+
 	this.content+'</p></div>';
 	
 	var that = this;
 	this.el.onclick = function(e) {
 		if(e.target.getAttribute("data-act")=="1") {
 		that.el.getElementsByClassName("long")[0].classList.toggle("hide");
+		e.target.classList.toggle("rotated");
 		}
 		
 	};
